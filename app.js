3b98d855a0e806d63c69316dc945b79e5c3025e0
CJ Moro <scjmoro@gmail.com>
2018-07-31 09:24:48 +0300

    Set up Node routing

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
 // import modules
 const http = require('http'),
-    fs = require('fs');
+    fs = require('fs'),
+    dotenv = require('dotenv').config();
+
+const walmartAPI = process.env.WALMARTAPI;
 
 // create a server
-const server = http.createServer(function(req, res) {
+const server = http.createServer(function (req, res) {
     console.log(`Request was made on ${req.url}`);
     // listen for a home directory route
     if (req.url === '/home' || req.url === '/') {
-        res.writeHead(200, {'content-type': 'text/html; charset=utf-8'});
+        res.writeHead(200, {
+            'content-type': 'text/html; charset=utf-8'
+        });
         fs.createReadStream(__dirname + '/public/index.html').pipe(res).on('error', errorHandler);
-    // upload js files
+        // upload js files
     } else if (req.url === '/public/js/search.js') {
-        res.writeHead(200, {'Content-type': 'text/javascript'});
+        res.writeHead(200, {
+            'Content-type': 'text/javascript'
+        });
         fs.createReadStream(__dirname + '/public/js/search.js').pipe(res).on('error', errorHandler);
         // upload css files - use different listening technique
     } else if (req.url.indexOf('.css') != -1) {
-        res.writeHead(200, {'Content-type': 'text/css'});
+        res.writeHead(200, {
+            'Content-type': 'text/css'
+        });
         fs.createReadStream(__dirname + '/public/stylesheets/styles.css').pipe(res).on('error', errorHandler);
+    } else if (req.url === '/walmart') {
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        res.end(JSON.stringify({name: 'hi'}));
     }
     // handle all non-existing routes
     else {
-        res.writeHead(404, {'Content-type': 'text/html; charset=utf-8'});
+        res.writeHead(404, {
+            'Content-type': 'text/html; charset=utf-8'
+        });
         res.end("Oops, that page doesn't exist... yet, maybe");
     }
 });
--- public/js/search.js
+++ public/js/search.js
@@ -1,4 +1,18 @@
 "use strict"
 window.onload = function() {
+    const searchBtn = document.querySelector('.search-btn');
+    searchBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        sendQuery('/walmart')
+    });
 
+    function sendQuery(endPoint) {
+        fetch(endPoint).then(function(res) {
+            return res.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        })
+        .catch(err => console.log(err));
+    }
 };
